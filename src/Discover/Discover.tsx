09e6e6fdf613a067bdb9e9089c4d57b8a4fe45df
Yongmin Kim <yongmin86k@gmail.com>
2021-11-01 16:57:35 -0700

    Add/create patch package

--- src/Discover/Discover.tsx
+++ src/Discover/Discover.tsx
@@ -158,16 +158,16 @@ const listenPrinter = async (printer: StarPrinter) => {
   } catch (error) {
     console.log(`Error: ${String(error)}`)
   } finally {
-    // await sleep(2000)
-    // await disconnectPrinter(printer)
+    await sleep(2000)
+    await disconnectPrinter(printer)
   }
 }
 
 const disconnectPrinter = async (printer: StarPrinter) => {
   try {
     console.log('-----> disconnectPrinter')
-    await printer.close()
-    await printer.dispose()
+    // await printer.close()
+    // await printer.dispose()
   } catch (error) {
     console.log(`Error: ${String(error)}`)
   }
